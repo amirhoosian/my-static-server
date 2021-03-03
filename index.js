758088e108f3ef7c3bb6b24d1343b27cf369932e
mrdcode78 <mrdcode78@gmail.com>
2021-03-03 17:48:16 +0330

    worn

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((req, res) => {
-    const fileName = path.join(process.cwd(), req.url)
+    const fileName = path.join(process.cwd(), `web//${req.url}`)
     try{
-        fs.lstatSync(fileName)
+     stat = fs.lstatSync(fileName)
     }catch{
-        res.writeHead(404, {"Conten-Type" : "text/html"})
+        res.writeHead(404, {"Content-Type" : "text/html"})
         res.write('not found')
        return res.end()
     }
 
+    if(stat.isFile()){
     fs.readFile(fileName, "UTF-8" ,(err, data) => {
         if(err) console.log(err)
-
+        res.writeHead(200, {"Content-Type" : "text/html"})
         res.write(data)
         res.end()
     })
+    return
+}else if(stat.isDirectory()){
+    res.writeHead(302, {"Location" : "/index.html"})
+    res.end
+}
 })
 
 
